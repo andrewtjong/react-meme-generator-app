f58f47a5473704b1708769f2cc617e005f4aef36
andrewtjong <andrewttj@yahoo.com>
2020-11-13 16:45:36 +0100

    updated header image

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header>
             <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                src="img/trollface.png" 
                 alt="Problem?"
             />
             <p>Meme Generator</p>
